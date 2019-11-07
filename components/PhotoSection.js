7b88636793977cce12139012d1dcbc2a33bc5831
dhiyo7 <dhiyoreksa12@gmail.com>
2019-11-07 20:10:43 +0700

    load feed & add Icon

--- components/PhotoSection.js
+++ components/PhotoSection.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Image } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
-export default class PhotoSection extends Component {
+class PhotoSection extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text> PhotoSection </Text>
+
+                {/* Username Section */}
+                <View style={styles.thumbnailSection}>
+                    <Image source={{uri: "https://i.pravatar.cc/400?img=1"}} style={styles.thumbnail}></Image>
+                    <Text style={styles.Username}> PhotoSection </Text>
+                </View>
+
+                {/* Photo Feed Section */}
+                <View>
+                    <Image source={{uri: "https://images.unsplash.com/photo-1553174241-0b28d763cafa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"}} style={styles.PhotosFeed}/>
+                </View>
+
+                {/* Ion Icon  */}
+                <View>
+                    <Icon name="ios-heart-empty" size={32} style={styles.Heart}/>
+                </View>
             </View>
         )
     }
@@ -13,6 +29,37 @@ export default class PhotoSection extends Component {
 
 const styles = StyleSheet.create({
     container:{
-        
+        flex: 1,
+    },
+
+    thumbnailSection:{
+        flexDirection:'row',
+        padding:5,
+        marginLeft: 10,
+        marginTop: 5,
+        alignItems:'center'
+    },
+
+    thumbnail:{
+        width:40,
+        height:40,
+        borderRadius:25,
+    },
+    Username:{
+        paddingLeft:7,
+        fontWeight:'bold',
+        fontSize:10,
+        letterSpacing:1
+    },
+    PhotosFeed:{
+        width:null,
+        height:250,
+    },
+    Heart:{
+        marginLeft:10,
+        padding: 5
+
     }
 })
+
+export default PhotoSection
---
+++ data.js
@@ -0,0 +1,40 @@
+ 
+ let data
+
+ export default data =  [
+               {
+                   id:1,
+                   username:"Dhiyo7",
+                   user_avatar:"https://i.pravatar.cc/400?img=1",
+                   image: "https://images.unsplash.com/photo-1553174241-0b28d763cafa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+                   caption:"#holiday,#Roam "
+               },
+               {
+                id:2,
+                username:"Plugin007",
+                user_avatar:"https://i.pravatar.cc/400?img=2",
+                image: "https://images.unsplash.com/photo-1546596183-0aa5a36b03b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+                caption:"#nightglow,#motel "
+            },
+            {
+                id:3,
+                username:"Krawit777",
+                user_avatar:"https://i.pravatar.cc/400?img=3",
+                image: "https://images.unsplash.com/photo-1560748369-d05f480b43bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+                caption:"#GoodMorning,#seeme "
+            },
+            {
+                id:4,
+                username:"Kuwuk070",
+                user_avatar:"https://i.pravatar.cc/400?img=4",
+                image: "https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+                caption:"#photoshoot, "
+            },
+            {
+                id:5,
+                username:"Goks",
+                user_avatar:"https://i.pravatar.cc/400?img=5",
+                image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+                caption:"#beauty,#selfie "
+            }
+    ]

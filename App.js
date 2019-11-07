44fc1ccd73427e45986608d385310519846a7903
dhiyo7 <dhiyoreksa12@gmail.com>
2019-11-07 18:15:25 +0700

    add Header

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet} from 'react-native'
+import Heading from './components/Heading'
 
 export class App extends Component {
   render() {
     return (
       <View style = {styles.container}>
-        <Text> My App </Text>
+        <Heading/>
       </View>
     );
   }
@@ -14,9 +15,7 @@ export class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c3e50',
+    backgroundColor: '#fff',
   }
 })
 
---
+++ components/Heading.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+class Heading extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}> Photos </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottomColor: '#ddd',
+        borderBottomWidth:2
+    },
+    text:{
+        fontSize:19,
+        fontWeight:'bold',
+        textAlign:'center',
+        margin: 13
+    }
+});
+
+export default Heading
